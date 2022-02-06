62bd5b531847deb3689a48246d4345556f759a88
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-02-06 12:41:03 +0300

    created the user model with four field and connected to the instance of db

--- app/models.py
+++ app/models.py
@@ -1,1 +1,11 @@
 from app import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200))
+
+class Post(db.Model):
+    pass
