9ac9fd3f1a3113869c2a1ce2c9e945d8ddeeeafe
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-02-06 15:38:40 +0300

    added validation functions to the forms.py file to raise an error to the user when the user selects an already taken username or email

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,9 @@
-from ast import Pass
-from flask import Flask
 from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
+from .models import model
+
+User = model.User
 
 class RegisterForm(FlaskForm):
     '''
@@ -14,22 +15,28 @@ class RegisterForm(FlaskForm):
     confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
-    # def validate_username(self):
-    #     '''
-    #     validate_username function for checking if the username is available for selection or it has already been taken
-    #     '''
-    #     pass
+    def validate_username(self, username):
+        '''
+        validate_username function for checking if the username is available for selection or it has already been taken
+        '''
+        user = User.query.filter_by(username = username.data).first()
+        if user:
+            raise ValidationError('Username already taken!')
 
-    # def validate_email(self):
-    #     '''
-    #     validate_email function for checking if the email is available for selection or it has already been taken
-    #     '''
-    #     pass
+        
+
+    def validate_email(self, email):
+        '''
+        validate_email function for checking if the email is available for selection or it has already been taken
+        '''
+        user = User.query.filter_by(email = email.data).first()
+        if user:
+            raise  ValidationError("Email already take!")
 
 class LoginForm(FlaskForm):
     '''
     Register form used for creating custom login form on the client side.
     '''
     email = EmailField("Email", validators=[DataRequired(), Email()])
-    password = PasswordField("Password", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired(),Length(min=6, max=50)])
     submit = SubmitField('Sign In')
