72a8cb29495f3ca944827bb183e15ef7911890cd
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-02-09 14:37:52 +0300

    changed the config.py file

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 class Config:
     pass
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI =  os.environ['SQLALCHEMY_DATABASE_URI']
+    SQLALCHEMY_DATABASE_URI =  'postgresql+psycopg2://moringa:blog@localhost/pitches'
 class DevConfig(Config):
     DEBUG = False
     # 'postgresql+psycopg2://moringa:blog@localhost:5433/pitches'
